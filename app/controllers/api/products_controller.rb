24559e0b2f5e03cceb57ad37aab3236a8e661729
Travis Knight <travis.a.knight@gmail.com>
2019-06-25 19:39:38 -0500

    Map method added. Does not work though.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,8 +1,11 @@
 class Api::ProductsController < ApplicationController
-
   def all_products
-  @products = Product.all
-  render "all_products.json.jb"
+    @products = Product.all
+    render "all_products.json.jb"
   end
 
+  def single_product
+    @products = Product.first
+    render "single_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
   namespace :api do
     get "/products" => "products#all_products"
+    get "/single_product" => "products#single_product"
   end
 end

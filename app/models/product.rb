331b53b39309c5d43774e3b1dd43929c5df75aa8
Travis Knight <travis.a.knight@gmail.com>
2019-06-30 10:27:42 -0500

    Add discount, tax, total methods to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    # if price < 30 then true else false end
+    # price < 30 ? true : false
+    price < 30
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    tax + price
+  end
 end
